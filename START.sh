0609f46957ed1f6209b5d7c8987ef76b5686231e
vaclav <vaclav@DESKTOP-HQA232F.home>
2026-08-13 21:45:55 +0200

    Enable full GPU hardware decoding (-hwaccel vaapi) to offload CPU during social media exports

--- START.sh
+++ START.sh
@@ -583,13 +583,23 @@ process_files() {
 
                 elif [ "$ENCODER_MODE" == "VAAPI" ]; then
                     run_ffmpeg_with_progress "$file" "AMD VAAPI export: $filename" "$out_mp4" \
-                        -vaapi_device /dev/dri/renderD128 \
+                        -hwaccel vaapi -hwaccel_device /dev/dri/renderD128 -hwaccel_output_format vaapi \
                         -y -i "$file" \
                         -map "0:v?" -map "0:a?" -map_chapters 0 \
-                        -vf 'format=nv12,hwupload' \
                         -c:v h264_vaapi -b:v "$target_bitrate" \
                         -c:a aac -b:a "$AUDIO_BITRATE" "$out_mp4"
                     ret=$?
+
+                    if [ $ret -ne 0 ]; then
+                        run_ffmpeg_with_progress "$file" "AMD VAAPI export (SW decode): $filename" "$out_mp4" \
+                            -vaapi_device /dev/dri/renderD128 \
+                            -y -i "$file" \
+                            -map "0:v?" -map "0:a?" -map_chapters 0 \
+                            -vf 'format=nv12,hwupload' \
+                            -c:v h264_vaapi -b:v "$target_bitrate" \
+                            -c:a aac -b:a "$AUDIO_BITRATE" "$out_mp4"
+                        ret=$?
+                    fi
                 fi
 
                 # Pokud HW akcelerace selhala nebo je režim CPU
